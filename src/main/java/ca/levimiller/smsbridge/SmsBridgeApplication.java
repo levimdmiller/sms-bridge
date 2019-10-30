4ad493076522cb2d73d457d0b628ca8e18319881
Levi Miller <levi.miller@qhrtech.com>
2019-10-29 19:04:54 -0700

    Initialize repo

---
+++ src/main/java/ca/levimiller/smsbridge/SmsBridgeApplication.java
@@ -0,0 +1,13 @@
+package ca.levimiller.smsbridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmsBridgeApplication {
+
+    public static void main( String[] args ) {
+        SpringApplication.run( SmsBridgeApplication.class, args );
+    }
+
+}
---
+++ src/test/java/ca/levimiller/smsbridge/SmsBridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.levimiller.smsbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmsBridgeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
