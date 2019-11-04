b36ff98f38858126375bf213028fa39b6cbde2ff
Levi Miller <levi.miller@qhrtech.com>
2019-11-04 01:13:13 -0800

    Hook up message service to rest controller, and disable unit tests until mocking is done

--- src/main/java/ca/levimiller/smsbridge/rest/impl/TwilioApi.java
+++ src/main/java/ca/levimiller/smsbridge/rest/impl/TwilioApi.java
@@ -1,6 +1,7 @@
 package ca.levimiller.smsbridge.rest.impl;
 
 import ca.levimiller.smsbridge.data.dto.TwilioSmsDto;
+import ca.levimiller.smsbridge.data.transformer.twilio.MessageTransformer;
 import ca.levimiller.smsbridge.rest.TwilioController;
 import ca.levimiller.smsbridge.service.MessageService;
 import javax.inject.Inject;
@@ -10,16 +11,20 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Component
 public class TwilioApi implements TwilioController {
+  private final MessageTransformer messageTransformer;
   private final MessageService messageService;
 
   @Inject
-  public TwilioApi(MessageService messageService) {
+  public TwilioApi(
+      MessageTransformer messageTransformer,
+      MessageService messageService) {
+    this.messageTransformer = messageTransformer;
     this.messageService = messageService;
   }
 
   @Override
   public void createSms(TwilioSmsDto sms) {
     log.info("Received sms: {}", sms);
-
+    messageService.save(messageTransformer.transform(sms));
   }
 }
